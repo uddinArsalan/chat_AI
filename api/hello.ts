df0363afda3421ca4ca6cfa8aa0d162c2fac1ae5
Arsalan <uddinarsalan91@gmail.com>
2025-02-27 18:35:49 +0530

    fix bugs

--- api/hello.ts
+++
@@ -1,4 +0,0 @@
-import { VercelResponse } from "@vercel/node";
-export default function handler(res: VercelResponse) {
-  res.status(200).json({ message: "Hello from the serverless function!" });
-}
